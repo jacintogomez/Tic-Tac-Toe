c1f9bc5f2155861b5727650857632dbb0f88f453
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-03-31 00:43:40 -0400

    Adding X's in function

--- src/App.js
+++ src/App.js
@@ -2,31 +2,33 @@
 import './index.css'
 import {useState} from 'react';
 
-function Square(){
-  const [value,setval]=useState(null);
-  function clickboard(){
-    setval('X');
-  }
-  return <button className="square" onClick={clickboard}>{value}</button>;
+function Square({value,clicktile}){
+  return <button className="square" onClick={clicktile}>{value}</button>;
 }
 
 export default function Board(){
+  const [squares,setsq]=useState(Array(9).fill(null));
+  function tileclick(i){
+    const nextsquares=squares.slice();
+    nextsquares[i]="X";
+    setsq(nextsquares);
+  }
   return(
       <>
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />
+          <Square value={squares[0]} clicktile={()=>tileclick(0)} />
+          <Square value={squares[1]} clicktile={()=>tileclick(1)}/>
+          <Square value={squares[2]} clicktile={()=>tileclick(2)}/>
         </div>
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />
+          <Square value={squares[3]} clicktile={()=>tileclick(3)}/>
+          <Square value={squares[4]} clicktile={()=>tileclick(4)}/>
+          <Square value={squares[5]} clicktile={()=>tileclick(5)}/>
         </div>
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />
+          <Square value={squares[6]} clicktile={()=>tileclick(6)}/>
+          <Square value={squares[7]} clicktile={()=>tileclick(7)}/>
+          <Square value={squares[8]} clicktile={()=>tileclick(8)}/>
         </div>
       </>
   );
