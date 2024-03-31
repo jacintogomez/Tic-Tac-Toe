a6ca01edb789bce0861b4ea06e0781bef79d17cf
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-03-31 15:25:06 -0400

    Functional game

--- src/App.js
+++ src/App.js
@@ -10,14 +10,19 @@ export default function Board(){
     const [squares,setsq]=useState(Array(9).fill(null));
     const [xup,setxup]=useState(true);
     function tileclick(i){
-
+        if(squares[i]||win(squares)){return;}
         const nextsquares=squares.slice();
         (xup)?nextsquares[i]="X":nextsquares[i]="O";
         setsq(nextsquares);
         setxup(!xup);
     }
+    const winner=win(squares);
+    let status;
+    if(winner){status="Winner is "+winner;}
+    else{status="Next player: "+(xup?"X":"O");}
     return(
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} clicktile={()=>tileclick(0)} />
                 <Square value={squares[1]} clicktile={()=>tileclick(1)} />
@@ -35,4 +40,24 @@ export default function Board(){
             </div>
         </>
     );
+}
+
+function win(squares){
+    const lines=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    for(let i=0;i<lines.length;i++){
+        const [a,b,c]=lines[i];
+        if(squares[a]&&squares[a]===squares[b]&&squares[a]===squares[c]){
+            return squares[a];
+        }
+    }
+    return null;
 }
